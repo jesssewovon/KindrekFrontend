51a471089ffc257345b12ed6ed1b683f43be9491
Djedje SEWOVON <jesssewovon@gmail.com>
2026-03-06 21:47:22 +0000

    Updates

--- src/moment/zh-cn.ts
+++ src/moment/zh-cn.ts
@@ -36,7 +36,7 @@ moment.defineLocale('zh-cn', {
             return hour >= 11 ? hour : hour + 12;
         }
     },
-    meridiem: function (hour, minute, isLower) {
+    meridiem: function (hour, minute) {
         var hm = hour * 100 + minute;
         if (hm < 600) {
             return '凌晨';
